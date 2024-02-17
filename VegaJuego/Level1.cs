9e03caa322d76ba79d62c090f814fd92951656a4
marduk9024 <ivan.sabater.moreno@ces-vegamedia.es>
2024-02-17 17:59:03 +0100

    El personaje se menea

--- VegaJuego/Level1.cs
+++ VegaJuego/Level1.cs
@@ -1,10 +1,26 @@
+using System.Diagnostics;
+using System.Media;
+
 namespace VegaJuego
 {
     public partial class Level1 : Form
     {
+        Link Player;
         public Level1()
         {
             InitializeComponent();
+            IniciarPersonajes();
+        }
+        private void IniciarPersonajes()
+        {
+            // Lienzo, coordX, coordY, salud, escudo y arma
+            Player = new Link(this, 0, 0, 30, 0, 0);
+        }
+
+        private void Level1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            Player.mover(e);
+            Debug.WriteLine("[X" + Player.Caja.Location.X + ",y" + Player.Caja.Location.Y + "]");
         }
     }
 }
